3ee4d048e5245ec5f10580f51bbc29ddab630148
KevinMateoG <gomezdiezkevinmateo@gmail.com>
2025-09-27 19:42:53 -0500

    if name en main.py

--- backend/app/main.py
+++ backend/app/main.py
@@ -2,6 +2,13 @@ from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from pydantic import BaseModel
 
+from typing import Union
+from fastapi import FastAPI, Request, Form
+from fastapi.templating import Jinja2Templates
+from fastapi.staticfiles import StaticFiles
+import json
+import uvicorn
+
 app = FastAPI()
 
 # Habilitar CORS
@@ -17,6 +24,9 @@ class Message(BaseModel):
     message: str
 
 @app.post("/chat")
-async def chat(message: Message):
+def chat(message: Message):
     # Aquí iría tu lógica de procesamiento del mensaje
     return {"response": f"Respuesta a: {message.message}"}
+
+if __name__ == '__main__':
+    uvicorn.run(app, host="127.0.0.1", port=8000)
