850733c56d87dff4a2bb0930e29dfde3ea23aad9
KevinMateoG <gomezdiezkevinmateo@gmail.com>
2025-09-28 20:11:51 -0500

    adaptacion del index de react a la pantalla correccion de errores

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,14 +1,17 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Index from './index';
 import Chatbot from './chatbot';
+import './App.css';
 
 function App() {
   return (
     <Router>
-      <Routes>
-        <Route path="/" element={<Index />} />
-        <Route path="/chatbot" element={<Chatbot />} />
-      </Routes>
+      <div className="app-container">
+        <Routes>
+          <Route path="/" element={<Index />} />
+          <Route path="/chatbot" element={<Chatbot />} />
+        </Routes>
+      </div>
     </Router>
   );
 }
