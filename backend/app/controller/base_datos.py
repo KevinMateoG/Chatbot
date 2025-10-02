f52139423fa5a43f3f7731943310c950b037be51
KevinMateoG <gomezdiezkevinmateo@gmail.com>
2025-10-01 22:24:06 -0500

    eliminacion de funcion en base_datos

--- backend/app/controller/base_datos.py
+++ backend/app/controller/base_datos.py
@@ -22,18 +22,6 @@ class BaseDatos:
         resultado = cursor.fetchone()
         return resultado
 
-    def guardar_encuesta(identificacion, datos_encuesta):
-        
-        # --- Lógica de la base de datos (Ejemplo de simulación) ---
-        print(f"✅ ¡Encuesta registrada! Usuario: {identificacion['numero']}")
-        for key, value in datos_encuesta.items():
-             print(f"- {key}: {value}")
-        # Aquí debes añadir el código de inserción real
-        # usando tu conector de base de datos (e.g., psycopg2, mysql.connector, SQLAlchemy, etc.)
-        # -----------------------------------------------------------
-
-        return "¡Gracias! Tu respuesta ha sido registrada exitosamente."
-
     def obtener_cursor():
         connection = psycopg2.connect(database=secretconfig.PGDATABASE, user=secretconfig.PGUSER, password=secretconfig.PGPASSWORD, host=secretconfig.PGHOST)
         cursor = connection.cursor()
