c9c94f8d67c943d51808e8d521d83073de0c8f2b
mercadot752-oss <mercadot752@gmail.com>
2025-10-11 20:57:56 +0000

    creacion lista de prioridad materias

--- backend/app/model/materia.py
+++ backend/app/model/materia.py
@@ -1,4 +1,3 @@
-from typing import str,int
 class Materia:
     def __init__(self,nombre:str,codigo:str,creditos:int):
         self.nombre = nombre
@@ -7,3 +6,31 @@ class Materia:
 
     def __repr__(self):
         return f"{self.nombre}, {self.codigo},({self.creditos} créditos"
+    
+    def prioridad (self):
+        if self.creditos >= 4:
+            return 'Alta'
+        elif 2 <= self.creditos < 4:
+            return 'Media'
+        else:
+            return 'Baja'
+        
+materia_procesadas = []
+
+materia_sin = [
+     
+    Materia("Calculo integral","calc123", 4),
+    Materia("Codigo Limpio","limp123", 1),
+    Materia("Estructuras","estru123", 3),
+    Materia("Física","fisi123", 5)
+    ]
+
+for i in materia_sin:
+    procesados = i.prioridad()
+    materia_procesadas.append((i.nombre,i.codigo,procesados))
+    
+print('Lista de prioridad de materias:')
+for nombre, codigo, prioridad in materia_procesadas: 
+    print(f"- {nombre} ({codigo}): prioridad {prioridad}")
+
+    
