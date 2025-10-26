c03eea45248af34641c68190a2e0c1b9cc711004
mercadot752-oss <mercadot752@gmail.com>
2025-10-26 13:41:44 +0000

    cambio menu lista de calificacion profesor

--- backend/app/model/profesor.py
+++ backend/app/model/profesor.py
@@ -1,9 +1,11 @@
 from usuario import Usuario
-from typing import Str,Int
+from typing import Str,Int,Dict,Any
 class Profesor(Usuario):
     def __init__(self, nombre: Str, apellido: Str, ide: Int, tipo_ide: Str):
         super().__init__(nombre, apellido, ide, tipo_ide)
-        self.materias_dict = {}
+        self.materias_dict:Dict [str,Any] = {}
+        self.link_hoja = "https://app.udem.edu.co/ServiciosEnLinea/login.html"
     
     def hoja_calculo (self):
+        return f"para calificar al profesor {self.nombre} {self.apellido}, por favor ingresa al link {self.link_hoja}"
         pass
